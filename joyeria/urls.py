b85370c7bfd50a59406b7762c161562914bfa73a
martinapalleiro <martinapalleiro26@gmail.com>
2025-11-01 17:15:35 -0300

    prueba

--- joyeria/urls.py
+++ joyeria/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health(_):
+    return HttpResponse("OK", content_type="text/plain")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+       path("", health, name="health"), 
     path("", include(("carrito.urls","carrito"), namespace="carrito")),
 ]
 
